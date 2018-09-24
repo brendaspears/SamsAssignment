5d47cb13691744fdbecc59ddd56a21c2dd66b3bd
brendaspears <brenda.young388@gmail.com>
2018-09-24 16:07:33 +0700

    Graph

---
+++ DrivingSimulationGraph.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+initialVelocity = 0
+speedLimit = 60
+timeSpent = float(input("Time Spent on the Road = "))
+acceleration = float(input("Acceleration = "))
+distance = float(input("Distance =  "))
+u = initialVelocity
+a = acceleration
+t = timeSpent
+v = u + ( a * t )
+x=[]
+y=[]
+for i in range ( int(timeSpent) ):
+    s = (1/2) * a * ((i)**2)
+    noOfStar = s // 5
+    print ("Duration : ",i," Distance : ",end="")
+    for j in range ( int(noOfStar)):
+        print("*",end="")
+    print ("\n")
+    x.append(i)
+    y.append(s)
+plt.plot(x,y)
+plt.show()
